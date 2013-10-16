e05c2fe1614237a8f07e74d14d26d6f02c20683c
wroy <wroy@uwo.ca>
2013-10-15 22:43:09 -0400

    adding jump and gravity effects

--- Assets/Scripts/FirstPersonController.cs
+++ Assets/Scripts/FirstPersonController.cs
@@ -1,17 +1,22 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+[RequireComponent (typeof(CharacterController))]
+
 public class FirstPersonController : MonoBehaviour {
 	
 	public float movementSpeed = 8;
 	public float lookSpeed = 4;
 	float verticalRotation = 0;
 	public float verticalLookRange = 60.0f;
+	float verticalVelocity = 0.0f;
+	public float jumpSpeed = 10;
+	CharacterController cc;
 
 	// Use this for initialization
 	void Start () {
 		Screen.lockCursor = true;
-	
+		cc = GetComponent<CharacterController>();
 	}
 	
 	// Update is called once per frame
@@ -22,8 +27,6 @@ public class FirstPersonController : MonoBehaviour {
 		transform.Rotate(0, rotX, 0);
 		
 		verticalRotation -= lookSpeed * Input.GetAxis ("Mouse Y");
-		//float currentVerticalAngle = Camera.main.transform.rotation.eulerAngles.x;
-		//float desiredVeritcalAngle = currentVerticalAngle - rotY;
 		verticalRotation = Mathf.Clamp(verticalRotation, -verticalLookRange, verticalLookRange);
 		Camera.main.transform.localRotation = Quaternion.Euler(verticalRotation,0,0);
 		
@@ -31,13 +34,19 @@ public class FirstPersonController : MonoBehaviour {
 		float forwardSpeed = movementSpeed * Input.GetAxis("Vertical");
 		float sideSpeed = movementSpeed * Input.GetAxis("Horizontal");
 		
-		Vector3 speed = new Vector3 (sideSpeed, 0,forwardSpeed);
+		verticalVelocity += 2 * Physics.gravity.y * Time.deltaTime;
+		
+		if (cc.isGrounded && Input.GetButton("Jump")){ //GetButtonDown is the anti bunny hop
+			verticalVelocity = jumpSpeed;	
+		}
+		
+		Vector3 speed = new Vector3 (sideSpeed, verticalVelocity,forwardSpeed);
 		
 		speed = transform.rotation * speed;
 		
-		CharacterController cc = GetComponent<CharacterController>();
 		
-		cc.SimpleMove(speed);
+		
+		cc.Move(speed * Time.deltaTime);
 	
 	}
 }
